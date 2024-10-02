8fab3481dd77bc7f3f5293cd2af3721b0aa46e08
WaelS-stack <wael.shahadeh1@marist.edu>
2024-10-02 19:50:03 -0400

    Add files via upload

---
+++ Labs/Lab 05/script.js
@@ -0,0 +1,35 @@
+//json loads then calls populates data table function
+window.fetch('data.json')
+    .then(response => response.json())
+    .then(data => populateTable(data))
+    .catch(error => console.error('Error loading JSON:', error));
+
+function populateTable(data) {
+    const tableBody = document.querySelector('#dataTable tbody');
+
+    data.forEach((person, index) => {
+        const row = document.createElement('tr');
+
+        if (index % 2 === 0) {
+            row.classList.add('evenRow');
+        } else {
+            row.classList.add('oddRow');
+        }
+
+        //just append the respective element to its respective cell
+
+        const idCell = document.createElement('td');
+        idCell.textContent = person.id;
+        row.appendChild(idCell);
+
+        const nameCell = document.createElement('td');
+        nameCell.textContent = person.name;
+        row.appendChild(nameCell);
+
+        const ageCell = document.createElement('td');
+        ageCell.textContent = person.age;
+        row.appendChild(ageCell);
+
+        tableBody.appendChild(row);
+    });
+}
