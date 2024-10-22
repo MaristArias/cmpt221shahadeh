16bd4ac47c4361e6245904a210a47d13a24f9974
WaelS-stack <wael.shahadeh1@marist.edu>
2024-10-22 17:26:16 -0400

    test

---
+++ Project/Client/public/js/index.js
@@ -0,0 +1,10 @@
+// Redirect to the home page when the spacebar is pressed or when the message is clicked
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+        window.location.href = '/home';
+    }
+});
+
+document.querySelector('.splash-message').addEventListener('click', function() {
+    window.location.href = '/home';
+});
