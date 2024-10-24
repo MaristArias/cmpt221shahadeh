7aef031ecdcec6a93123e40ec904e09e61bb2191
damansingh27 <152332917+damansingh27@users.noreply.github.com>
2024-10-23 23:39:28 -0400

    upd

--- Project/Client/public/js/registration.js
+++ Project/Client/public/js/registration.js
@@ -1,3 +1,13 @@
+function cloneArray(data) {
+  data.forEach((item) => tableData.push(item));
+}
+
+window
+  .fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => populateTable(data))
+  .catch((error) => console.error("Error loading JSON:", error));
+
 function goBack() {
   const form = document.getElementById("myForm");
   form.noValidate = true;
